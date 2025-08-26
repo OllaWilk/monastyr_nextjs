1e0034969af4fb987ba55848023d6a1f91f48d63
OllaWilk <alex.dev.wilk@gmail.com>
2025-08-26 12:33:25 +0200

    b

--- components/IntentionForm/IntentionForm.tsx
+++ components/IntentionForm/IntentionForm.tsx
@@ -1,26 +1,20 @@
 "use client";
 
-import { useState } from "react";
-
 export const IntentionForm = () => {
-  const [intention, setIntention] = useState("");
-
   async function handleSubmit(e: React.FormEvent) {
     e.preventDefault();
-    console.log(intention);
+    console.log("intention");
   }
 
   return (
-    <form onSubmit={handleSubmit}  noValidate>
+    <form onSubmit={handleSubmit} noValidate>
       <input type="text" name="website" />
 
-      <label htmlFor="intention" >
-        Treść intencji
-      </label>
+      <label htmlFor="intention">Treść intencji</label>
       <textarea
         id="intention"
         placeholder="Napisz swoją intencję…"
-        value={intention}
+        value={'intention'}
       />
 
       <button type="submit">"Wyślij intencję"</button>
