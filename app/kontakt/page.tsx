62d1833f7104e883e7b0cb99b8eaa37bde7ef10d
OllaWilk <alex.dev.wilk@gmail.com>
2025-09-01 14:10:53 +0200

    del styles from kontakt page.tsx

--- app/kontakt/page.tsx
+++ app/kontakt/page.tsx
@@ -1,4 +1,3 @@
-'use client';
 
 import React from "react";
 
@@ -7,25 +6,24 @@ import { headerData } from "@/data/kontakt/header";
 import { Paragraph } from "@/components/Paragraph/Paragraph";
 import { Titles } from "@/components/Titles/Titles";
 import { kontaktData } from "@/data/kontakt/kontakt";
-import styles from "./kontakt.module.scss"
 
 export default function Kontakt() {
   const { form } = kontaktData;
 
 
   return (
-    <div className={styles.page}>
+    <div >
       <SubpageHeader data={headerData} />
       <section
-        className={styles.wrapper}
+       
         aria-label="Kontakt i intencje modlitewne"
       >
-        <div className={styles.card}>
+        <div >
           <Titles as="h2" title={form.title} />
           <Paragraph text={form.description} />
         </div>
 
-        <aside className={styles.aside}>
+        <aside>
           <h3>Dołącz do nas w social media</h3>
           <p>Sprawdź aktualności, zdjęcia i krótkie rozważania.</p>
         
