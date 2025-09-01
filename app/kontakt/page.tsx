6c72d9cf31e5d02d01b506f741dd789142a4a49f
OllaWilk <alex.dev.wilk@gmail.com>
2025-09-01 14:38:30 +0200

    add styles to kontatk page

--- app/kontakt/page.tsx
+++ app/kontakt/page.tsx
@@ -1,32 +1,28 @@
-
 import React from "react";
-
 import { SubpageHeader } from "@/components/SubpageHeader/SubpageHeader";
 import { headerData } from "@/data/kontakt/header";
 import { Paragraph } from "@/components/Paragraph/Paragraph";
 import { Titles } from "@/components/Titles/Titles";
 import { kontaktData } from "@/data/kontakt/kontakt";
-
+import styles from "./kontakt.module.scss";
 export default function Kontakt() {
   const { form } = kontaktData;
 
-
   return (
-    <div >
+    <div className={styles.page}>
       <SubpageHeader data={headerData} />
       <section
-       
+        className={styles.wrapper}
         aria-label="Kontakt i intencje modlitewne"
       >
-        <div >
+        <div className={styles.card}>
           <Titles as="h2" title={form.title} />
           <Paragraph text={form.description} />
         </div>
 
-        <aside>
+        <aside className={styles.aside}>
           <h3>Dołącz do nas w social media</h3>
           <p>Sprawdź aktualności, zdjęcia i krótkie rozważania.</p>
-        
         </aside>
       </section>
     </div>
